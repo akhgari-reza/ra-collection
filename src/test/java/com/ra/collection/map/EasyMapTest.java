2dff0961a615aa522512e25e6cf466d711adab37
Reza Akhgari <r.akhgari@mail.ru>
2021-02-12 20:44:32 +0330

    unit test exapnding

--- src/test/java/com/ra/collection/map/EasyMapTest.java
+++ src/test/java/com/ra/collection/map/EasyMapTest.java
@@ -50,4 +50,22 @@ public class EasyMapTest {
 
 
     }
+
+    @Test
+    public void testSortingGivenHashMapByIdUsingTreeMap_thenAssertTrue()
+    {
+        Map<String, String> map = new HashMap<>();
+        map.put("j", "java");
+        map.put("c", "c++");
+        map.put("p", "python");
+        map.put("n", "node");
+
+        // Sort map using EasyMap
+        Map<String , String> sortedMap = EasyMap.sortByKeyUsingTreeMap(map);
+
+        //Assert
+        Map.Entry<String , String> expectedItem = Map.entry("c", "c++");
+        Assertions.assertEquals(4, sortedMap.entrySet().size());
+        Assertions.assertEquals( expectedItem , sortedMap.entrySet().toArray()[0]);
+    }
 }
